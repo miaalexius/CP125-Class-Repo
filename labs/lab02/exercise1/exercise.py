7cbabc036b46913c924dcdf69d4ead2890dbc32e
miaalexius <miadaziy@icloud.com>
2025-12-08 12:03:27 +0800

    commit

--- labs/lab02/exercise1/exercise.py
+++ labs/lab02/exercise1/exercise.py
@@ -2,9 +2,13 @@
 # Write your code below:
 
 def is_budget_sufficient(one_way_km, km_per_liter, price_per_liter, budget):
-    # TODO: Implement this function
-    # Calculate round trip cost and checks if within budget
-    pass
+    
+    real_budget = ((one_way_km * 2)/km_per_liter) * price_per_liter
+    
+    if budget >= real_budget:
+        return "Enough"
+    else :
+        return "Not enough"
 
 # Test your code here
 print("Testing Road Trip Budgeter...")
--- labs/lab02/exercise2/exercise.py
+++ labs/lab02/exercise2/exercise.py
@@ -4,9 +4,10 @@
 import math
 
 def calculate_event_cost(participants, tent_capacity, tent_price, meal_price):
-    # TODO: Implement this function
-    # Calculate total cost for tents and meals
-    pass
+    total_tent_price = (participants / tent_capacity) * tent_price
+    cost_meal_price = meal_price * participants
+    total_budget = total_tent_price + cost_meal_price
+    return total_budget
 
 # Test your code here
 print("Testing Camping Logistics...")
