bdec8df74fc1ba8249f4efb1c7d4f0a8767b200d
miaalexius <miadaziy@icloud.com>
2025-12-29 13:06:25 +0800

    yey

--- labs/lab02/exercise10/exercise10.py
+++ labs/lab02/exercise10/exercise10.py
@@ -1,22 +1,19 @@
 
 def calculate_base_usage(distance):
-    """
-    Calculates the base battery usage.
-    1.5% battery per 10 meters.
-    """
-    # TODO: Implement this function
-    pass
+    usage = distance / 10
+    return usage * 1.5
 
 def apply_mode_bonus(usage, is_sport_mode):
-    """
-    Increases battery consumption by 50% if in Sport Mode.
-    """
-    # TODO: Implement this function
-    pass
+    if is_sport_mode:
+        return usage * 1.5  
+    
+    else : 
+        return usage 
 
 def has_enough_battery(distance, current_battery, is_sport_mode):
-    """
-    Calculates if there is enough battery for a round trip (distance * 2).
-    """
-    # TODO: Implement this function
-    pass
+    round_trip_distance = distance * 2
+
+    base_usage = calculate_base_usage(round_trip_distance)
+    total_usage = apply_mode_bonus(base_usage, is_sport_mode)
+
+    return current_battery >= total_usage
