64c1b6a5155a6b376282e81f5b01bd286cfe8932
miaalexius <miadaziy@icloud.com>
2026-01-12 11:18:18 +0800

    commit

--- labs/lab03/exercise1/exercise1.py
+++ labs/lab03/exercise1/exercise1.py
@@ -1,2 +1,8 @@
 def count_bright_spots(pixels):
-    pass
+    bright_spot = 0
+    for i in range (1,len(pixels)-1): 
+
+        if pixels[i] > pixels[i-1] and pixels[i] > pixels[i+1]: 
+            bright_spot = bright_spot + 1
+
+    return bright_spot
--- labs/lab03/exercise2/exercise2.py
+++ labs/lab03/exercise2/exercise2.py
@@ -1,5 +1,20 @@
 def find_station(stations, name):
-    pass
+    for i in range (len(stations)): 
+        if name == stations[i]:
+            return i 
+    return -1 
 
 def count_stops(stations, start, stop):
-    pass
+    start_index = find_station(stations, start)
+    stop_index = find_station (stations, stop)
+
+    if start_index == -1 or stop_index == -1: 
+        return -1
+        
+    if start_index == stop_index : 
+        return 0 
+    
+    else: 
+        count = stop_index - start_index
+        return abs(count)  
+    
